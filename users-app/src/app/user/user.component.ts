23ca3d1a6cf17d284b71fae34d8d8d12567fe88a
Atssini Cabrera <atssinicabrera@Atssinis-MacBook-Pro.local>
2018-05-03 17:55:39 -0700

    first not done

---
+++ users-app/src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  users: Array<User> = [
+    new User(1, "First", "Last", "email@email.com"),
+    new User(2, "First", "Last", "email@email.com"),
+    new User(3, "First", "Last", "email@email.com")
+
+  ];
+
+  constructor(private _userServive: UserService) { }
+
+  ngOnInit() {
+    //this.getUsers();
+  }
+
+  getUsers(){
+    //this._userServive.getUsers()
+    //.then(users => this.users = users)
+  }
+}
